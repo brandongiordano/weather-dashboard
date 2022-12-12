cf73a31473c03ae5fa6c9526f33b2a214d3d2eed
brandongiordano <bgiordano9917@aol.com>
2022-12-12 17:11:42 -0500

    added bottom cards for 5 day forecast

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+//weather variable selectors
 var tempEl = document.getElementById("city-temp");
 var humidEl = document.getElementById("city-humid");
 var windEl = document.getElementById("wind-speed");
+
+//API call
+https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={c1cca8b9a19bd1602dafeedbadde768a}
+
+
