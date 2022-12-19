4d77d5032a28245f8d92de4acd8c2376e76ce625
brandongiordano <bgiordano9917@aol.com>
2022-12-19 10:46:07 -0500

    5 day functioning

--- scripts.js
+++ scripts.js
@@ -7,19 +7,39 @@ var iconEl = $("#weather-icon");
 //var searchButton = $("#search-button");
 //var searchFormEl = document.querySelector('#search-bar');
 //var searchedCity = document.querySelector("#search-input").value;
-var lat = 40.7831;
-var lon = -73.9712;
+var searchedCity = "Boston"
+//var cityLat = (cityData.coord.lat);
+var cityLat = 40.7831;
+//var cityLon = (cityData.coord.lon);
+var cityLon = -73.9712;
 var APIKey="c1cca8b9a19bd1602dafeedbadde768a";
 
-//display current day
+//display current date
 var today = dayjs().format('M/D/YYYY');
 $('#todays-date').text(today);
 
+function getCity() {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + APIKey;
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (cityData) {
+            getCoords(cityData)
+        });
+}
+
+getCoords = function (cityData) {
+    console.log(cityData);
+
+
+}
 
 //get weather function
 let weather = {
     fetchWeather: function(){
-        fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey)
+        fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&appid=" + APIKey)
         .then(function (response) {
             return response.json();
         })
@@ -38,21 +58,21 @@ displayWeather = function (data) {
     $(tempEl).append(Math.round((data.list[0].main.temp - 273.15) * 1.80 + 32) + "°F");
     $(humidEl).append(data.list[0].main.humidity + "%");
     $(windEl).append(data.list[0].wind.speed + " km/h");
-    //$(iconEl).attr({"src": "http://openweathermap.org/img/wn/" + data.list[0].weather.icon + ".png",
-    //"height": "100px", "width":"100px"});
-}
+    var weatherIcon = (data.list[0].weather.icon);
+    var iconUrl = "http://openweathermap.org/img/wn/" + weatherIcon + ".png";
+    $(cityEl).append("<img src=" + iconUrl + ">");
+    console.log(weatherIcon);
 
-//get future forecasts
-forecast = function(data) {
     for (i=0;i<5;i++){
         var date = new Date((data.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
         var iconCode = data.list[((i+1)*8)-1].weather[0].icon;
-        var iconurl ="https://openweathermap.org/img/wn/"+iconCode+".png";
+        var iconurl ="http://openweathermap.org/img/wn/"+iconCode+"@2x.png";
         var tempK = data.list[((i+1)*8)-1].main.temp;
         var tempF =(((tempK-273.5)*1.80)+32).toFixed(2);
         var humidity = data.list[((i+1)*8)-1].main.humidity;
         var windF = (data.list[i].wind.speed + " km/h")
     
+        //populate forecast cards
         $("#date"+i).append(date);
         $("#img"+i).append("<img src="+iconurl+">");
         $("#temp"+i).append(tempF+"°F");
@@ -62,3 +82,4 @@ forecast = function(data) {
 }
 
 weather.fetchWeather();
+getCity();
