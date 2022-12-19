172df6270d8bd45c1bd8a78efffa93710cd5a8fd
brandongiordano <bgiordano9917@aol.com>
2022-12-18 23:35:28 -0500

    altered HTML to fit preview

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 //weather variable selectors
 var cityEl = $("#city-name");
-var tempEl = $("#city-temp");
-var humidEl = $("#city-humid");
+var tempEl = $("#current-temp");
+var humidEl = $("#current-humid");
 var windEl = $("#wind-speed");
 var iconEl = $("#weather-icon");
-var searchCityEl = $("#search-input");
+//var searchButton = $("#search-button");
+//var searchFormEl = document.querySelector('#search-bar');
+//var searchedCity = document.querySelector("#search-input").value;
 var lat = 40.7831;
 var lon = -73.9712;
 var APIKey="c1cca8b9a19bd1602dafeedbadde768a";
@@ -27,17 +29,34 @@ let weather = {
     }
 };
 
+//fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + searchedCity + ",US&limit=5&appid=" + APIKey)
 
 //populating text fields
-var displayWeather = function (data) {
+displayWeather = function (data) {
     console.log(data);
     $(cityEl).append(data.city.name);
     $(tempEl).append(Math.round((data.list[0].main.temp - 273.15) * 1.80 + 32) + "°F");
     $(humidEl).append(data.list[0].main.humidity + "%");
     $(windEl).append(data.list[0].wind.speed + " km/h");
-    $(iconEl).attr({"src": "http://openweathermap.org/img/wn/" + data.list[0].weather.icon + ".png",
-    "height": "100px", "width":"100px"});
+    //$(iconEl).attr({"src": "http://openweathermap.org/img/wn/" + data.list[0].weather.icon + ".png",
+    //"height": "100px", "width":"100px"});
 }
 
+//get future forecasts
+forecast = function(data) {
+    for (i=0;i<5;i++){
+        var date= new Date((data.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+        var iconCode= data.list[((i+1)*8)-1].weather[0].icon;
+        var iconurl="https://openweathermap.org/img/wn/"+iconCode+".png";
+        var tempK= data.list[((i+1)*8)-1].main.temp;
+        var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
+        var humidity= data.list[((i+1)*8)-1].main.humidity;
+    
+        $("#date"+i).append(date);
+        $("#img"+i).append("<img src="+iconurl+">");
+        $("#temp"+i).append(tempF+"°F");
+        $("#humidity"+i).append(humidity+"%");
+    }
+}
 
 weather.fetchWeather();
